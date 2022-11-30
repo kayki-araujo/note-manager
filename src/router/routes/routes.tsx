572d7249d01e84068ee4c5665ccfd5702032749a
kayki.garcia <kayki.garcia@lighthouseit.com.br>
2022-11-29 22:14:40 -0300

    refactor: add router basename

--- src/router/routes/routes.tsx
+++ src/router/routes/routes.tsx
@@ -13,42 +13,45 @@ import {
   User,
 } from './lazy';
 
-const routes = createBrowserRouter([
-  {
-    path: '/',
-    element: <MainLayout />,
-    errorElement: <NotFound />,
-    children: [
-      {
-        path: '/',
-        element: <Feed />,
-      },
-      {
-        path: '/create',
-        element: <Create />,
-      },
-      {
-        path: '/note/:noteId',
-        element: <Note />,
-      },
-      {
-        path: '/note/:noteId/edit',
-        element: <EditNote />,
-      },
-      {
-        path: '/user',
-        element: <User />,
-      },
-    ],
-  },
-  {
-    path: '/sign-in',
-    element: <SignIn />,
-  },
-  {
-    path: '/sign-up',
-    element: <SignUp />,
-  },
-]);
+const routes = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <MainLayout />,
+      errorElement: <NotFound />,
+      children: [
+        {
+          path: '/',
+          element: <Feed />,
+        },
+        {
+          path: '/create',
+          element: <Create />,
+        },
+        {
+          path: '/note/:noteId',
+          element: <Note />,
+        },
+        {
+          path: '/note/:noteId/edit',
+          element: <EditNote />,
+        },
+        {
+          path: '/user',
+          element: <User />,
+        },
+      ],
+    },
+    {
+      path: '/sign-in',
+      element: <SignIn />,
+    },
+    {
+      path: '/sign-up',
+      element: <SignUp />,
+    },
+  ],
+  { basename: '/note-manager/' }
+);
 
 export const Routes: FC = () => <RouterProvider router={routes} />;
